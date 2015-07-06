224faeb87bf56001b99848641b04f0eec9ed5e66
Stuart Lees <stuartlees@gmail.com>
2015-07-06 18:03:19 -0400

    Base files and infrastructure
    - formatting config files
    - bower and package files added with dependencies specified
    - circle ci build file added
    - gulp file with all necessary dependencies and tasks defined
    - web component test config file added (`wct.conf.js`)
    - `rise-google-sheet.html` component file added with base initialization
    - test infrastructure added with initial unit and integration files set up and ready for tests to be added

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+(function (console) {
+  "use strict";
+
+  var gulp = require("gulp");
+  var bump = require("gulp-bump");
+  var jshint = require("gulp-jshint");
+  var rename = require("gulp-rename");
+  var uglify = require('gulp-uglify');
+  var colors = require("colors");
+  var runSequence = require("run-sequence");
+  var wct = require("web-component-tester").gulp.init(gulp);
+  var bower = require("gulp-bower");
+  var del = require("del");
+
+  gulp.task("clean-bower", function(cb){
+    del(["./bower_components/**"], cb);
+  });
+
+  gulp.task("lint", function() {
+    return gulp.src("./*.html")
+      .pipe(jshint.extract("always"))
+      .pipe(jshint())
+      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jshint.reporter("fail"));
+  });
+
+  // ***** Primary Tasks ***** //
+  gulp.task("bower-clean-install", ["clean-bower"], function(cb){
+    return bower().on("error", function(err) {
+      console.log(err);
+      cb();
+    });
+  });
+
+  gulp.task("bump", function(){
+    return gulp.src(["./package.json", "./bower.json"])
+      .pipe(bump({type:"patch"}))
+      .pipe(gulp.dest("./"));
+  });
+
+  gulp.task("test", function(cb) {
+    runSequence("test:local", cb);
+  });
+
+  gulp.task("build", function (cb) {
+    runSequence("lint", cb);
+  });
+
+  gulp.task("default", [], function() {
+    console.log("********************************************************************".yellow);
+    console.log("  gulp bower-clean-install: delete and re-install bower components".yellow);
+    console.log("  gulp bump: increment the version".yellow);
+    console.log("  gulp test: run unit and integration tests".yellow);
+    console.log("  gulp build: build component".yellow);
+    console.log("********************************************************************".yellow);
+    return true;
+  });
+
+})(console);
