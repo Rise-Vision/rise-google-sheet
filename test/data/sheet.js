e8d1c6d8dfe353b9967243ab89e60be7ba5d7620
Stuart Lees <stuartlees@gmail.com>
2015-07-08 11:52:11 -0400

    Retrieving data
    - `rise-google-sheet.html`
    	- using `<iron-ajax>` for making request to Sheets API
    	- defined private constant `SCOPE` to define base URL for Sheets API
    	- added properties `key`, `tabId`, and `cells`
    	- added `prepareResponse` function to configure the response object with necessary data to return with
    	- added `_onSheetError` and `_onSheetResponse` functions to handle request responses
    	- `go()` function updated to configure API URL and generate request
    - bower dependencies updated
    - test coverage
    	- data files added for mocking data returned from a Sheets API request
    	- unit and integration tests added for functionality added
    - demo file added to demonstrate usage so far

---
+++ test/data/sheet.js
@@ -0,0 +1,281 @@
+var sheetData = {
+  "encoding": "UTF-8",
+  "feed": {
+    "author": [
+      {
+        "email": {
+          "$t": "stuartlees@risevision.com"
+        },
+        "name": {
+          "$t": "stuart.lees"
+        }
+      }
+    ],
+    "category": [
+      {
+        "scheme": "http://schemas.google.com/spreadsheets/2006",
+        "term": "http://schemas.google.com/spreadsheets/2006#cell"
+      }
+    ],
+    "entry": [
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "Description",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "Description",
+          "col": "1",
+          "row": "1"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C1"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C1",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "A1",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      },
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "Number",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "Number",
+          "col": "2",
+          "row": "1"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C2"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C2",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "B1",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      },
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "Type",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "Type",
+          "col": "3",
+          "row": "1"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C3"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R1C3",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "C1",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      },
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "Apple",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "Apple",
+          "col": "1",
+          "row": "2"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C1"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C1",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "A2",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      },
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "123",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "123",
+          "col": "2",
+          "row": "2"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C2"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C2",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "B2",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      },
+      {
+        "category": [
+          {
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#cell"
+          }
+        ],
+        "content": {
+          "$t": "Fruit",
+          "type": "text"
+        },
+        "gs$cell": {
+          "$t": "Fruit",
+          "col": "3",
+          "row": "2"
+        },
+        "id": {
+          "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C3"
+        },
+        "link": [
+          {
+            "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/R2C3",
+            "rel": "self",
+            "type": "application/atom+xml"
+          }
+        ],
+        "title": {
+          "$t": "C2",
+          "type": "text"
+        },
+        "updated": {
+          "$t": "2015-05-19T20:37:34.428Z"
+        }
+      }
+    ],
+    "gs$colCount": {
+      "$t": "26"
+    },
+    "gs$rowCount": {
+      "$t": "1000"
+    },
+    "id": {
+      "$t": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values"
+    },
+    "link": [
+      {
+        "href": "https://docs.google.com/spreadsheets/d/abc123/pubhtml",
+        "rel": "alternate",
+        "type": "application/atom+xml"
+      },
+      {
+        "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values",
+        "rel": "http://schemas.google.com/g/2005#feed",
+        "type": "application/atom+xml"
+      },
+      {
+        "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values",
+        "rel": "http://schemas.google.com/g/2005#post",
+        "type": "application/atom+xml"
+      },
+      {
+        "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values/batch",
+        "rel": "http://schemas.google.com/g/2005#batch",
+        "type": "application/atom+xml"
+      },
+      {
+        "href": "https://spreadsheets.google.com/feeds/cells/abc123/1/public/values?alt=json",
+        "rel": "self",
+        "type": "application/atom+xml"
+      }
+    ],
+    "openSearch$startIndex": {
+      "$t": "1"
+    },
+    "openSearch$totalResults": {
+      "$t": "6"
+    },
+    "title": {
+      "$t": "Test Sheet",
+      "type": "text"
+    },
+    "updated": {
+      "$t": "2015-05-19T20:37:34.428Z"
+    },
+    "xmlns": "http://www.w3.org/2005/Atom",
+    "xmlns$batch": "http://schemas.google.com/gdata/batch",
+    "xmlns$gs": "http://schemas.google.com/spreadsheets/2006",
+    "xmlns$openSearch": "http://a9.com/-/spec/opensearchrss/1.0/"
+  },
+  "version": "1.0"
+};
